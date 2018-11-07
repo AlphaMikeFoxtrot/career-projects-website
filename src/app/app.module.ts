0b2c36e9dea544a90a6730073bc09dc535afc21e
FardeenPanjwani <panjwanifardeen@gmail.com>
2018-11-07 19:06:54 +0530

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectListItemComponent } from './project-list/project-list-item/project-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProjectListComponent,
+    ProjectListItemComponent
   ],
   imports: [
     BrowserModule,
